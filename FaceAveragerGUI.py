8f80d1da1af626dd837ba3f3006266a4e33c42b2
ShehabMuhammad <47852641+ShehabMuhammad@users.noreply.github.com>
2021-02-28 17:21:22 +0200

    Update FaceAveragerGUI.py

--- FaceAveragerGUI.py
+++ FaceAveragerGUI.py
@@ -12,40 +12,46 @@ class Averager():
         self.root = tk.Tk();
         self.button1 = tk.Button(self.root, text="save the image.", command=self.Merge).pack()
         self.button2 = tk.Button(self.root, text="Add a face.", command=self.add).pack()
-        self.label = tk.Label(self.root, text="Please choose two frontal face images, With no swayings").pack(side="bottom")
-        self.width = 240;
-        self.height = 320;
+        self.label = tk.Label(self.root, text="Please choose the images you'd like to merge.").pack(side="bottom")
+        self.width = 200;
+        self.height = 200;
+        self.count=1;
         self.faceSpace = np.empty([self.width, self.height, 3])
-    def add(self, face):
+    def add(self):
         gi1 = cv2.imread(askopenfilename());
-
         gi1 = cv2.cvtColor(gi1, cv2.COLOR_BGR2GRAY)
-
-        face1 = detector.detectMultiScale(gi1, 1.1, 5);
-        if len(face1) == 0 or len(face2) == 0:
+        face1 = detector.detectMultiScale(gi1);            
+        if len(face1) == 0:
             raise "No Faces Have been Found."
         
-        print(f" Number of faces found were : {len(face1)} and {len(face2)}")
+        print(f" Number of faces found were : {len(face1)} ")
         (x1, y1, X1, Y1) = face1[0];
-        gi1 = gi1[x1+y1:X1-Y1]
-        #gi2 = gi2[x2+y2:X2-Y2]            
-        gi1 = cv2.resize(gi1, (self.width, self.height), 3);
+        print(f"x1,y1,X1,Y1 : {x1,y1,X1,Y1}" )
+        gi1 = gi1[y1:X1+Y1]
+        #gi2 = gi2[x2+y2:X2-Y2]
+    
+        try:
+            gi1 = cv2.resize(gi1, (self.width, self.height), 3);
+        except Exception as e:
+            print(" This image is invalid, choose another one."); return;
     
         imgArray = np.resize(np.array(gi1), (self.width, self.height, 3));
-        self.faceSpace = (self.faceSpace + imgArray)/2
+        self.count += 1;
+        self.faceSpace = (self.faceSpace + imgArray)/self.count
         
     def Merge(self):
       
         
         fileName = time.strftime("%Y_%m_%d_%H_%M_%S")
-        
         cv2.imwrite('img' + fileName + '.png', self.faceSpace)
-        
+        self.faceSpace = np.empty([self.width, self.height, 3])
+        self.count=1;
         cv2.imshow("That's an Image.", cv2.imread(f"img{fileName}.png"));
 
 
 
 if __name__ == '__main__':
+    print("Please Wait.. ")
     obj = Averager()
 
         
